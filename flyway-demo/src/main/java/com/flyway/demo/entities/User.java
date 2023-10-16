12b6f2cfcaca6773132b3ce1216e205aee8d8766
Rushibhawar <rushikeshbhawar1213@gmail.com>
2023-10-16 15:52:56 +0530

    Created the flyway versioned migrations configs and scripts

--- flyway-demo/src/main/java/com/flyway/demo/entities/User.java
+++ flyway-demo/src/main/java/com/flyway/demo/entities/User.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @Entity
-@Table(name = "user")
+@Table(name = "user_table")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ flyway-demo/src/main/resources/db/migration/V1__init_database.sql
@@ -0,0 +1,35 @@
+CREATE TABLE "user" (
+  id UUID NOT NULL,
+   username VARCHAR(255) NOT NULL,
+   password VARCHAR(255) NOT NULL,
+   email VARCHAR(255),
+   CONSTRAINT pk_user PRIMARY KEY (id)
+);
+
+CREATE TABLE project (
+  id UUID NOT NULL,
+   end_date TIMESTAMP WITHOUT TIME ZONE,
+   name VARCHAR(255) NOT NULL,
+   start_date TIMESTAMP WITHOUT TIME ZONE,
+   manager_id UUID,
+   CONSTRAINT pk_project PRIMARY KEY (id)
+);
+
+
+CREATE TABLE task (
+  id UUID NOT NULL,
+   name VARCHAR(255),
+   start_date TIMESTAMP WITHOUT TIME ZONE,
+   end_date TIMESTAMP WITHOUT TIME ZONE,
+   assignee_id UUID,
+   project_id UUID,
+   CONSTRAINT pk_task PRIMARY KEY (id)
+);
+
+ALTER TABLE "user" ADD CONSTRAINT uc_user_email UNIQUE (email);
+
+ALTER TABLE project ADD CONSTRAINT FK_PROJECT_ON_MANAGER FOREIGN KEY (manager_id) REFERENCES "user" (id);
+
+ALTER TABLE task ADD CONSTRAINT FK_TASK_ON_ASSIGNEE FOREIGN KEY (assignee_id) REFERENCES "user" (id);
+
+ALTER TABLE task ADD CONSTRAINT FK_TASK_ON_PROJECT FOREIGN KEY (project_id) REFERENCES project (id);
